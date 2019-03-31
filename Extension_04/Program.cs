675e4ea4899ed5303b0cf7850da61f9ba3f0c836
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 13:13:19 +0400

    Extension with Recursion (simple)

---
+++ Extension_04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_04
+{
+    static class ExtensionClass
+    {// Recursion (simple).
+        public static void ExtensionMethod(this string value, int counter)
+        {
+            counter--;
+            Console.WriteLine(value+counter);
+
+            if(counter!=0)
+            {
+                value.ExtensionMethod(counter); 
+            }
+            Console.WriteLine(value+counter);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "Hello";
+            text.ExtensionMethod(3);
+
+            Console.ReadLine();
+        }
+    }
+}
