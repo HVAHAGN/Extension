67216c1b6db5af95125fabb37186ff3090eec774
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 12:44:56 +0400

    Create simple extensin example

--- Extension/Program.cs
+++ Extension/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace Extension
 {
+    // Extend methods. (Extension methods)
+
+    // Extending methods can only be static and be created only in static classes.
+
+    static class ExtensionClass
+    {
+        // this - tells the compiler that this method is an extension!
+
+        public static void ExtensionMethod(this string value)
+        {
+            Console.WriteLine(value);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            string text = "Text hello";
+
+            // Call the method as static.
+            ExtensionClass.ExtensionMethod(text);
+
+            // Call the method as extending.
+               text.ExtensionMethod();
+
+
+
+
             Console.ReadLine();
         }
     }
